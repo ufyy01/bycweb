a640dbe6203daa8c78922686a80c0752d3886a22
ufy <ufuomaitwa@gmail.com>
2023-12-29 22:16:47 +0100

    Header completed

---
+++ script.js
@@ -0,0 +1,18 @@
+function searchbar() {
+    const searchBtn = document.getElementById("search-btn");
+    const searchDiv = document.querySelector(".search-div");
+    const brandLogo = document.querySelector(".big-logo");
+
+    searchBtn.style.display = "none"
+    brandLogo.style.display = "none"
+    searchDiv.style.display = "block"
+}
+function closeSearch() {
+    const searchBtn = document.getElementById("search-btn");
+    const searchDiv = document.querySelector(".search-div");
+    const brandLogo = document.querySelector(".big-logo");
+
+    searchBtn.style.display = "inline-block";
+    brandLogo.style.display = "block";
+    searchDiv.style.display = "none";
+}
